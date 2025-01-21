77da496482e4fbeb232c5f3816ae77a0f13eecec
LukeMJTD <luke@mjtd.org>
2025-01-21 15:17:57 +0100

    added cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,14 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['https://www.whaleden.com']
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
